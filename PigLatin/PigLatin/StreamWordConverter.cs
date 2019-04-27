34fb29d64c775c824900633587f36c84e5f03977
Ruslan Mukhamedov <predictor@yandex.ru>
2019-04-27 19:43:41 +0200

    Added StreamWordConverter

---
+++ PigLatin/PigLatin/StreamWordConverter.cs
@@ -0,0 +1,63 @@
+﻿namespace PigLatin
+{
+    using System;
+    using System.IO;
+    using System.Text;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    public class StreamWordConverter : IDisposable
+    {
+        private readonly StreamReader streamReader;
+        private readonly Func<string, string> convertWord;
+        private readonly Func<char, bool> isWordSeparator;
+
+        public StreamWordConverter(Stream stream, Func<string, string> convertWord, Func<char, bool> isWordSeparator)
+        {
+            this.streamReader = new StreamReader(stream);
+            this.convertWord = convertWord;
+            this.isWordSeparator = isWordSeparator;
+        }
+
+        public async Task ConvertTo(Stream stream, CancellationToken cancellationToken)
+        {
+            var streamWriter = new StreamWriter(stream);
+            var builder = new StringBuilder();
+            while (true)
+            {
+                cancellationToken.ThrowIfCancellationRequested();
+                Memory<char> memory = new Memory<char>(new char[1]);
+                var count = await this.streamReader.ReadAsync(memory, cancellationToken);
+                if (count == 0)
+                {
+                    if (builder.Length > 0)
+                    {
+                        var converted = this.convertWord(builder.ToString());
+                        await streamWriter.WriteAsync(converted.AsMemory(), cancellationToken);
+                    }
+                    return;
+                }
+                char c = memory.ToArray()[0];
+                if (isWordSeparator(c))
+                {
+                    if(builder.Length > 0)
+                    {
+                        var converted = this.convertWord(builder.ToString());
+                        await streamWriter.WriteAsync(converted.AsMemory(), cancellationToken);
+                        builder = new StringBuilder();
+                    }
+                    await streamWriter.WriteAsync(memory, cancellationToken);
+                }
+                else
+                {
+                    builder.Append(c);
+                }
+            }
+        }
+
+        public void Dispose()
+        {
+            streamReader?.Dispose();
+        }
+    }
+}
--- PigLatin/PigLatinTests/PigLatinTests.cs
+++ PigLatin/PigLatinTests/PigLatinTests.cs
@@ -3,6 +3,11 @@ namespace PigLatinTests
     using Microsoft.VisualStudio.TestTools.UnitTesting;
 
     using PigLatin;
+    using System.IO;
+    using System.Reflection;
+    using System.Text;
+    using System.Threading;
+    using System.Threading.Tasks;
 
     [TestClass]
     public class PigLatinTests
@@ -18,6 +23,20 @@ namespace PigLatinTests
         public void BasicTestCases(string original, string pigLatin)
         {
             Assert.AreEqual(pigLatin, PigLatinWordConverter.Convert(original));
+        }
+
+        [TestMethod]
+        public async Task StreamWordConverter_WithDegenerateConverter()
+        {
+            string path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"Resources\gpl.txt");
+            string text = File.ReadAllText(path);
+            var converter = new StreamWordConverter(new MemoryStream(Encoding.UTF8.GetBytes(text)), s => s, CharExtensions.IsWordSeparator);
+            var convertedStream = new MemoryStream();
+            await converter.ConvertTo(convertedStream, CancellationToken.None);
+            convertedStream.Position = 0;
+            var convertedText = await new StreamReader(convertedStream).ReadToEndAsync();
+            File.WriteAllText(path + ".pig", convertedText);
+            Assert.AreEqual(text, convertedText);
         }
     }
 }
