eeecccdbfd810ae626b8776d4aa614b69ff05228
Ruslan Mukhamedov <predictor@yandex.ru>
2019-04-28 17:19:01 +0200

    Azure App Service

---
+++ PigLatin/PigLatin.Service/Startup.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using System;
+using System.IO;
+
+namespace PigLatin.Service
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            app.Run(async context =>
+            {
+                if(context.Request.Method.Equals("GET", StringComparison.OrdinalIgnoreCase))
+                {
+                    using (var writer = new StreamWriter(context.Response.Body))
+                    {
+                        await writer.WriteAsync("Welcome to Pig Latin text converter. Send the text to convert in a POST request.".AsMemory(), context.RequestAborted);
+                    }
+                }
+
+                if (context.Request.Method.Equals("POST", StringComparison.OrdinalIgnoreCase))
+                {
+                    context.Response.ContentType = "text/plain";
+                    using (var outStream = context.Response.Body)
+                    {
+                        using (var converter = new StreamWordConverter(context.Request.Body, PigLatinWordConverter.Convert, CharExtensions.IsWordSeparator, CharExtensions.IsNotLatinLetter))
+                        {
+                            await converter.ConvertTo(outStream, context.RequestAborted);
+                        }
+                    }
+                }
+            });
+        }
+    }
+}
